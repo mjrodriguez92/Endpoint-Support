7d13db32a35a68f74b88a712973df6fc6121b71d
mjrodriguez92 <mrodr287727@gmail.com>
2024-11-21 16:05:14 -0600

    Added PSScriptInfo

--- ExtractAutopilotHWID.ps1
+++ ExtractAutopilotHWID.ps1
@@ -1,3 +1,27 @@
+<#PSScriptInfo
+
+.VERSION 1.0
+
+.AUTHOR Michael Rodriguez
+
+
+.RELEASENOTES
+Version 1.0:  Original published version.
+Version 1.1: Added PSScriptInfo Section
+
+#>
+
+<#
+.SYNOPSIS
+Retreives all the information related to importing endpoints into the Intune Autopilot repository.
+
+.DESCRIPTION
+This script retreives all the necessary data such as the device manufacturer, serial numer, and hardware hash
+that is exported to csv to allow device imports into the Autopilot registered devices repository in Intune.
+
+#>
+
+
 New-Item -Type Directory -Path "C:\HWID"
 Set-Location -Path "C:\HWID"
 $env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
